02515cbda52639da59b780347f1f412baef6a8e7
A00259393-Vamsi <A00259393@mycambrian.ca>
2022-11-01 14:06:19 -0400

    Adding new project to git repository with basic layout

---
+++ CAnyWhere/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CAnyWhere;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ CAnyWhere/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CAnyWhere;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
