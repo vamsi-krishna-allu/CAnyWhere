54f1ff954dac552650e029285da83db9393c6e40
nareshaspire <Nareshaspire45@gmail.com>
2022-11-06 12:05:21 -0500

    models added both Users and for userfiles, each model contains different fields as emailID,name,password and user type in user models. UserFiles model is added with various fields using string , LikeFileID,UserID,FileName and FileLocation. Used this keyword for both userID and UserFiles models.

---
+++ CAnyWhere/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAnyWhere.Models
+{
+    internal class User
+    {
+        public string EmailId { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string UserType { get; set; }
+
+        public User(string Id, string Name, string Password, string UserType)
+        {
+            this.EmailId = Id;
+            this.Name = Name;
+            this.Password = Password;
+            this.UserType = UserType;
+        }
+    }
+}
---
+++ CAnyWhere/Models/UserFiles.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAnyWhere.Models
+{
+    internal class UserFiles
+    {
+        public string FileId { get; set; }
+        public string UserId { get; set; }
+        public string FileName { get; set; }
+        public string FileLocation { get; set; }
+
+        public UserFiles(string FileId, string UserId, string FileName, string FileLoaction)
+        {
+            this.FileId = FileId;
+            this.UserId = UserId;
+            this.FileName = FileName;
+            this.FileLocation = FileLoaction;
+        }
+    }
+}
