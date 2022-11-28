6ac37243a68200c1106e72198d6f982bb812948e
A00259393-Vamsi <A00259393@mycambrian.ca>
2022-11-27 20:01:47 -0500

    CAnywhere: Added passwordValidator to validate the password entered by the user Below are the conditions added for validation 1. verify Minimum 6 characters and maximum 20 characters 2. verify if password contain Atleast One Upper Case 3. verify if password contain Atleast One Lower Case 4. verify if password contain Atleast One Special Character 5. verify if password doesnot contain spaces

---
+++ CAnyWhere/Validators/PasswordValidator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAnyWhere.Validators
+{
+    internal class PasswordValidator : CustomValidator
+    {
+        public bool Validate(object value)
+        {
+            if(value != null)
+            {
+                String password = value.ToString();
+                if (password.Length < 6 || password.Length > 20)
+                    return false;
+
+                if (!password.Any(char.IsUpper))
+                    return false;
+
+                if (!password.Any(char.IsLower))
+                    return false;
+
+                if (password.Contains(' '))
+                    return false;
+
+                string specialCharacters = @"%!@#$%^&*()?/>.<,:;'\|}]{[_~`+=-" + "\"";
+                char[] specialCharacterArray = specialCharacters.ToCharArray();
+                foreach (char ch in specialCharacterArray)
+                {
+                    if (password.Contains(ch))
+                        return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
