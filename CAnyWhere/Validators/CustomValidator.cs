128052f4fb3ca875b83ae11f6faad94f67562599
A00259393-Vamsi <A00259393@mycambrian.ca>
2022-11-27 19:47:30 -0500

    CAnyWhere: Added a custom validator. Main purpose of this interface is to create a model based on which all the validator classes can be implemented in future As part of this interface declared a method Validate The validate method has to be impleemnted in the custom validators to validate the data provided

---
+++ CAnyWhere/Validators/CustomValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAnyWhere.Validators
+{
+    internal interface CustomValidator
+    {
+        Boolean Validate(object value);
+    }
+}
