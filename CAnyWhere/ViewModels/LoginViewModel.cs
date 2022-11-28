235c33b8044e8b7ae54cb170b0d1a9ee4636a90b
A00259393-Vamsi <A00259393@mycambrian.ca>
2022-11-27 20:12:49 -0500

    CAnywhere: password validate and error in loginViewModel Added code to validate the password and throw an error as an alert if the password is invalid Alert message shows all the requirements for valid pasword It returns true if the password is valid

--- CAnyWhere/ViewModels/LoginViewModel.cs
+++ CAnyWhere/ViewModels/LoginViewModel.cs
@@ -16,7 +16,17 @@ namespace CAnyWhere.ViewModels
                 return false;
             }
 
-            
+            customValidator = new PasswordValidator();
+            isValid = customValidator.Validate(password);
+
+            if (!isValid)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error",
+                    String.Join(Environment.NewLine, "Password is Invalid.", "1. Minimum 6 characters and maximum 20 characters",
+                    "2. Should Contain Atleast One Upper Case", "3. Should Contain Atleast One Lower Case",
+                    "4. Should Contain Atleast One Special Character", "5. Should not contain spaces"), "OK");
+                return false;
+            }
 
             return true;
         }
