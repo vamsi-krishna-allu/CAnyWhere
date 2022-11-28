7c1ddbd49bbe581c5bdb87d42c6922b9d090a254
A00259393-Vamsi <A00259393@mycambrian.ca>
2022-11-27 20:08:21 -0500

    CAnywhere: LoginViewModel Added LoginViewModel to validate the login functionality Added condition that verifies if the username is valid or not Shows an alert if username is invalid and return true if the email is valid

---
+++ CAnyWhere/ViewModels/LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CAnyWhere.Validators;
+using System.Web;
+
+namespace CAnyWhere.ViewModels
+{
+    internal class LoginViewModel
+    {
+        public static async Task<bool> OnLogin(string username, string password)
+        {
+            CustomValidator customValidator = new EmailValidator();
+            bool isValid = customValidator.Validate(username);
+
+            if (!isValid)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "Username is Invalid. Please try Again", "OK");
+                return false;
+            }
+
+            
+
+            return true;
+        }
+    }
+}
