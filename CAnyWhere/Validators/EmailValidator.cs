fadbdcb040d4044385c039ef564c427328fdb2ee
A00259393-Vamsi <A00259393@mycambrian.ca>
2022-11-27 19:57:10 -0500

    CAnyWhere: Added emailValidator that implements CustomValidator. As part of EmailValidation I used EmailAddressAttribute class from System.ComponentModel.DataAnnotations to validate the email provided by the user As part of IsValid method it would check for null  and would check if it has only one @ sysmbol and verifies if the symbol is not the first and last character

---
+++ CAnyWhere/Validators/EmailValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAnyWhere.Validators
+{
+    internal class EmailValidator : CustomValidator
+    {
+        public bool Validate(object value)
+        {
+            return new EmailAddressAttribute().IsValid(value);
+        }
+    }
+}
