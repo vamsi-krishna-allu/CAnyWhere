7e041aef30fafc452c46f84d4cfd8d78d904f14f
nareshaspire <Nareshaspire45@gmail.com>
2022-11-28 00:06:20 -0500

    CAnywhere: Added New Model It is added to show the list of Data objects n the home page.It holds the data like the image source, posted date , like and dislike count

---
+++ CAnyWhere/Models/DashBoardData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAnyWhere.Models
+{
+    public class DashBoardData
+    {
+
+        public string SourceUrl { get; set; }
+        public string PostedDate { get; set; }
+        public string Description { get; set; }
+        public string LikeCount { get; set; }
+        public string DislikeCount { get; set; }
+    }
+}
