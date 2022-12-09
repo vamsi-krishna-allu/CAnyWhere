065c94e6cb9881669958793074dc2ac616354977
A00259393-Vamsi <A00259393@mycambrian.ca>
2022-12-09 12:54:46 -0500

    CAnyhwere:
    Added changes for new page
    This page holds four buttons for different operations
    operations supported are
    pick image
    pick video
    capture image
    capture video

---
+++ CAnyWhere/Views/PostVideoScreen.xaml.cs
@@ -0,0 +1,40 @@
+﻿using CAnyWhere.Services;
+
+namespace CAnyWhere.Views;
+
+public partial class PostVideoScreen : ContentPage
+{
+    private string username;
+	public PostVideoScreen(string username)
+	{
+		InitializeComponent();
+        this.username = username;
+	}
+
+    private void onPostPickedVideo(object sender, EventArgs e)
+    {
+        FileStorageService FileStorageService = new();
+        FileStorageService.postPickedVideoAsync(username);
+
+    }
+
+    private void onPostCapturedVideo(object sender, EventArgs e)
+    {
+        FileStorageService FileStorageService = new();
+        FileStorageService.postCapturedVideoAsync(username);
+
+    }
+
+    private void onPostPickedImage(object sender, EventArgs e)
+    {
+        FileStorageService FileStorageService = new();
+        FileStorageService.postPickedPhotoAsync(username);
+    }
+
+    private void onPostCapturedImage(object sender, EventArgs e)
+    {
+        FileStorageService FileStorageService = new();
+        FileStorageService.postCapturedPhotoAsync(username);
+    }
+}
+
